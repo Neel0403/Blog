30e53c1fed22f32b12a0a8f68bd45b820bd39624
Nidhi <neelo2003@gmail.com>
2024-04-29 21:02:25 +0530

    Fix bug in logout

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -45,7 +45,7 @@ export class AuthService {
         try {
             await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
+            console.log("Appwrite service :: logout :: error", error);
         }
     }
 
